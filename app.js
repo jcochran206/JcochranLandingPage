fb97c82420e9419b68d057f27bfa64ec7953f935
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-03-16 19:23:01 -0700

    added pages to project and tested routes to pages

---
+++ app.js
@@ -0,0 +1,34 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+//////////////////// routes ////////////////////
+app.get('/', function(req, res){
+  res.render("home");
+});
+
+app.get('/about', function(req, res){
+  res.render("about");
+});
+
+
+app.get('/project', function(req, res){
+  res.render("project");
+});
+
+app.get('/contact', function(req, res){
+  res.render("contact");
+});
+////////////////// app listening on port //////////////////////////
+app.listen(port, function(){
+  console.log(`server started and is listening on ${port}`);
+});
